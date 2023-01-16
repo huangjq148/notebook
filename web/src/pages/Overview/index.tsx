e7185d71e157503bf8f766fc71d5c2f38d008839
huangjianqiang <huangjianqiang@xmfunny.com>
2023-01-16 18:12:31 +0800

    概览页面优化

--- web/src/pages/Overview/index.tsx
+++ web/src/pages/Overview/index.tsx
@@ -1,100 +1,95 @@
-import { SearchForm } from "@/components"
-import { getTop5Data } from '@/services/overview'
-import { Button, DatePicker, Form, List } from 'antd'
-import dayjs from 'dayjs'
-import { useEffect, useState } from 'react'
-import styles from "./index.module.less"
+import { SearchForm, DateRangePicker } from "@/components";
+import { getTop5Data } from "@/services/overview";
+import { Button, DatePicker, Form, List } from "antd";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import styles from "./index.module.less";
 
 type TopData = {
-    name: string;
-    money: string;
-}
+  name: string;
+  money: string;
+};
 
 export default () => {
-    // const [conditions, setConditions] = useState({})
-    const [topData, setTopData] = useState<any>({})
+  const [topData, setTopData] = useState<any>([]);
 
+  const loadTopData = async (conditions?: Record<string, any>) => {
+    const result = (await getTop5Data(conditions)) as any;
 
-    const loadTopData = async (conditions?: Record<string, any>) => {
-        const result = await getTop5Data(conditions) as any
-        setTopData(result)
+    const newData = [
+      {
+        label: "购买最多的产品",
+        data: result.top5BuyGoods || [],
+      },
+      {
+        label: "卖的最多的产品",
+        data: result.top5SellGoods || [],
+      },
+      {
+        label: "利润最高的产品",
+        data: result.top5ProfitGoods || [],
+      },
+      {
+        label: "利润最多的客户",
+        data: result.top5BuyCustomer || [],
+      },
+    ];
+    console.log(newData);
+    setTopData(newData);
+  };
+
+  useEffect(() => {
+    loadTopData();
+  }, []);
+
+  const handleFormSearch = (values: any) => {
+    let conditions = {};
+
+    if (values.orderTime?.length) {
+      conditions = {
+        startOrderTime: dayjs(values.orderTime[0]).format("YYYY-MM-DD"),
+        endOrderTime: dayjs(values.orderTime[1]).format("YYYY-MM-DD"),
+      };
     }
 
-    useEffect(() => { loadTopData() }, [])
+    loadTopData(conditions);
+  };
 
-    const handleFormSearch = (values: any) => {
-        let conditions = {}
+  return (
+    <div>
+      <SearchForm>
+        <Form onFinish={handleFormSearch} layout="inline">
+          <Form.Item label="日期" name="orderTime">
+            <DateRangePicker />
+          </Form.Item>
 
-        if (values.orderTime?.length) {
-            conditions = {
-                startOrderTime: dayjs(values.orderTime[0]).format("YYYY-MM-DD"),
-                endOrderTime: dayjs(values.orderTime[1]).format("YYYY-MM-DD")
-            }
-        }
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              查询
+            </Button>
+          </Form.Item>
+        </Form>
+      </SearchForm>
 
-        loadTopData(conditions)
-    }
-
-    return <div>
-        <SearchForm>
-            <Form onFinish={handleFormSearch} layout="inline">
-                <Form.Item label="日期" name="orderTime">
-                    <DatePicker.RangePicker
-                        presets={[
-                            { label: "今天", value: [dayjs(), dayjs()] },
-                            { label: "上周", value: [dayjs().add(-7, "day").day(1), dayjs().add(-7, "day").day(7)] },
-                            { label: "本周", value: [dayjs().day(1), dayjs().day(7)] },
-                            { label: "上月", value: [dayjs().add(-1, "month").startOf("month"), dayjs().add(-1, "month").endOf("month")] },
-                            { label: "本月", value: [dayjs().startOf("month"), dayjs().endOf("month")] },
-                        ]} format={val => dayjs(val).format("YYYY-MM-DD")} allowClear placeholder={["起始日期", "结束日期"]} />
-                </Form.Item>
-
-                <Form.Item>
-                    <Button htmlType='submit' type="primary">查询</Button>
-                </Form.Item>
-            </Form>
-        </SearchForm>
-        <div className={styles.topListWrapper}>
-            <List
-                header={<div>购买最多的产品</div>}
-                bordered
-                dataSource={topData.top5BuyGoods ?? []}
-                renderItem={(item: TopData) => (
-                    <List.Item>
-                        <><span>{item.name}</span> {parseFloat(item.money).toFixed(2)}</>
-                    </List.Item>
-                )}
-            />
-            <List
-                header={<div>卖的最多的产品</div>}
-                bordered
-                dataSource={topData.top5SellGoods ?? []}
-                renderItem={(item: TopData) => (
-                    <List.Item>
-                        <><span>{item.name}</span> {parseFloat(item.money).toFixed(2)}</>
-                    </List.Item>
-                )}
-            />
-            <List
-                header={<div>利润最高的产品</div>}
-                bordered
-                dataSource={topData.top5ProfitGoods ?? []}
-                renderItem={(item: TopData) => (
-                    <List.Item>
-                        <><span>{item.name}</span> {parseFloat(item.money).toFixed(2)}</>
-                    </List.Item>
-                )}
-            />
-            <List
-                header={<div>利润最多的客户</div>}
-                bordered
-                dataSource={topData.top5BuyCustomer ?? []}
-                renderItem={(item: TopData) => (
-                    <List.Item>
-                        <><span>{item.name}</span> {parseFloat(item.money).toFixed(2)}</>
-                    </List.Item>
-                )}
-            />
-        </div>
+      <div className={styles.topListWrapper}>
+        {topData.map((topDataItem: any) => (
+          <List
+            className={styles.listDataContainer}
+            key={topDataItem.label}
+            header={<div>{topDataItem.label}</div>}
+            bordered
+            dataSource={topDataItem.data ?? []}
+            renderItem={(item: TopData) => (
+              <List.Item>
+                <>
+                  <span>{item.name}</span>
+                  <span>{parseFloat(item.money).toFixed(2)}</span>
+                </>
+              </List.Item>
+            )}
+          />
+        ))}
+      </div>
     </div>
-}
+  );
+};
