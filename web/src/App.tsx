382e19ffac1998431df62e808ad263dd1668d985
huangjianqiang <huangjianqiang@xmfunny.com>
2025-06-19 10:51:20 +0800

    refactor(web): 文件结构调整

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,10 @@
-import "./App.css";
-import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
-import { Watermark } from "antd";
-import { useState } from "react";
-import { useUser } from "./store";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const { userInfo } = useUser((state) => ({
-    userInfo: state.userInfo,
-  }));
   return (
-    <Watermark content={`${userInfo.name}(${userInfo.username})`} style={{ height: "100%" }}>
       <RouterProvider router={router} />
-    </Watermark>
   );
 }
 
--- web/src/layout/index.tsx
+++ web/src/layout/index.tsx
@@ -2,18 +2,22 @@ import { UserOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
 
 import { DesktopOutlined, FileOutlined, PieChartOutlined } from "@ant-design/icons";
-import { Layout, Menu, MenuProps } from "antd";
+import { Layout, Menu, MenuProps, Watermark } from "antd";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import LayoutHeader from "./components/Header";
 import styles from "./index.module.less";
 
 import SideMenu from "./components/Menu";
+import { useUser } from "@/store";
 
 const { Sider, Content } = Layout;
 
 const App: React.FC = () => {
   const navigate = useNavigate();
   const [collapsed, setCollapsed] = useState(false);
+  const { userInfo } = useUser((state) => ({
+    userInfo: state.userInfo,
+  }));
 
   return (
     <Layout style={{ width: "100vw", height: "100vh" }}>
@@ -31,7 +35,9 @@ const App: React.FC = () => {
             overflow: "auto",
           }}
         >
-          <Outlet />
+          <Watermark content={`${userInfo.name}(${userInfo.username})`} style={{ height: "100%" }}>
+            <Outlet />
+          </Watermark>
         </Content>
       </Layout>
     </Layout>
