ff53ee294ced036a4bf074d47755f2365293c36c
huangjq148 <583075772@qq.com>
2025-06-22 13:13:32 +0800

    feat: 读数

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,19 +1,18 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-    }
-  }
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
 ]);
