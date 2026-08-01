0ec55f13e004b846e880c4214cd784d1f347b24e
huangjq148 <583075772@qq.com>
2026-08-01 08:33:01 +0800

    fix: 修复日期选择器星期等文案为中文（改用 antd ESM locale 并激活 dayjs zh-cn）

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,11 @@
 import { ConfigProvider } from 'antd';
 import ReactDOM from 'react-dom/client';
+import dayjs from 'dayjs';
 import App from './App';
 import './index.css';
-import Zh from 'antd/locale/zh_CN';
+import Zh from 'antd/es/locale/zh_CN';
 import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
 import { Inspector } from 'react-dev-inspector';
 // https://ant-design.antgroup.com/docs/react/v5-for-19-cn
 import '@ant-design/v5-patch-for-react-19'
